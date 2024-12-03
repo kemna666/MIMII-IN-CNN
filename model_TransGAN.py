c8f86c32ec33895f7eea6ea1126335f18b05176f
kemna@666gmail.com <kemna666@gmail.com>
2024-12-03 17:44:43 +0800

    模型写好了（可能算是吧）

--- model_TransGAN.py
+++ model_TransGAN.py
@@ -1,8 +1,5 @@
-#导入transformer处理音频的库
-from transformers import Wav2Vec2Processor
 import torch.nn as nn
 import torch
-import torch.optim as optim
 
 #Transformer模型
 class TransAudio(nn.Module):
@@ -58,3 +55,38 @@ class Discriminator(TransAudio):
         vaildity =  torch.sigmoid(self.output_layer(output))
         return vaildity
     
+#正式定义TransGAN
+class TransGAN(nn.Module):
+    def __init__(self,generator,discriminator):
+        super().__init__()
+        self.generator = generator
+        self.discriminator = discriminator
+    
+    def train(self, noise, real_audio, optimizer_generator, optimizer_disciminator, device):
+        #训练生成器
+        #梯度归零
+        self.generator.zero_grad()
+        #生成假数据
+        fake_audio = self.generator(noise)
+        #detach()作用是防止梯度回传
+        fake_discriminator= self.discriminator(fake_audio.detach())
+        #损失函数
+        #鼓励生成器生成的数据被判别器判断为真实数据，从而提高生成数据的质量
+        loss_generator = -torch.mean(torch.log(fake_discriminator + 1e-6))
+        #反向传播
+        loss_generator.backward()
+        optimizer_generator.step()
+
+        #训练判别器
+        self.discriminator.zero_grad()
+        real_discriminator = self.discriminator(real_audio)
+        loss_discriminator_real = -torch.mean(torch.log(real_discriminator + 1e-6))
+        fake_discriminator = self.discriminator(fake_audio)
+        loss_discriminator_fake = -torch.mean(torch.log(1 - fake_discriminator + 1e-6))
+        loss_discriminator = (loss_discriminator_real + loss_discriminator_fake)/2
+        loss_discriminator.backward()
+        optimizer_disciminator.step()
+
+        #.item()方法用于从只包含一个元素的张量中提取值，并返回该值，同时保持元素的数据类型不变
+        return loss_generator.item(), loss_discriminator.item()
+
---
+++ train.py
@@ -0,0 +1,33 @@
+from model_TransGAN import Generator,Discriminator,TransGAN
+from Dataset import MIMIIDataset
+import torch.optim as optim
+import torch.nn as nn 
+import torch
+import pickle
+
+#定义模型
+generator = Generator(input_dim=100,heads_num=4,dim_feedforward=2048,num_layers=2,num_classes=256)
+discriminator = Discriminator(input_dim=256,heads_num=4,dim_feedforward=2048,num_layers=2,num_classes=1)
+model = TransGAN(generator,discriminator)
+train_loader,val_loader,test_loader = MIMIIDataset.features_exteract()
+# 定义损失函数和优化器
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+
+#定义训练参数
+#训练轮数
+num_epochs = 100
+
+#开始训练
+for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+        for data in train_loader:  # 假设你已经有了一个DataLoader
+            inputs, labels = data
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        print(f'Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}')
